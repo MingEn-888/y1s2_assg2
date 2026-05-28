b6325ba97bf338d3bce5e2acf3e8f114d4f4f972
jordan0118 <jordangong888@gmail.com>
2026-05-28 14:28:04 +0800

    main 1.0

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include "File.h"
+#include "Folder.h"
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;

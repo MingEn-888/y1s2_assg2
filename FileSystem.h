254ac024d47516aace0152dc2360f01858d9c79d
zisuan <lolkning3225@gmail.com>
2026-05-31 00:11:09 +0800

    main formation and robust validation input

---
+++ FileSystem.h
@@ -0,0 +1,30 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Folder.h"
+#include "File.h"
+using namespace std;
+
+class FileSystem {
+private:
+    Folder* root;
+    Folder* currentFolder;
+    
+public:////
+    FileSystem();
+    ~FileSystem();
+    bool createFolder(const std::string& name);
+    bool createFile(const std::string& name, const std::string& extension);
+    void displayCurrentFolder();
+    void displayFullTree();
+    std::vector<std::string> searchFile(const std::string& name, const std::string& extension);
+    bool enterFolder(const std::string& name);
+    bool goBackToParentFolder();
+    bool deleteFile(const std::string& name, const std::string& extension);
+    bool deleteFolder(const std::string& name);
+    void showCurrentPath();
+};
+
+#endif
